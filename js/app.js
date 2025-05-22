49b645fc04d482930529a7536cf4db6b4e2245e1
MOON-0101 <aarddog6@gmail.com>
2025-05-22 12:41:44 +0900

    0522

---
+++ js/app.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const progressBar = document.querySelectorAll(".progress-bar")
+    progressBar.forEach(bar => {
+        const target = bar.getAttribute('data-bs-target')
+        setTimeout(() =>{
+            bar.style.width = target + '%';
+        },500)
+    })
+
+    const searchInput = document.getElementById("searchInput")
+    const portfolioItems = document.querySelectorAll(".portfolio-item")
+    searchInput.addEventListener("input", function() {
+        const searchTerm = this.value.toLowerCase();
+        portfolioItems.forEach((item) =>{
+            const title = item.querySelector("h4").textContent.toLowerCase();
+            if(title.includes(searchTerm)) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        }) 
+    })
+
+
+    const filterbutton = document.querySelectorAll(".filter-btn");
+    filterbutton.forEach((button) => {
+        button.addEventListener("click", function() {
+        const filter = this.getAttribute("data-filter");
+            portfolioItems.forEach((item) => {
+                if(filter === 'all' || item.getAttribute("data-category") === filter) {
+                item.style.display = "block";
+                } else {
+                item.style.display = "none";
+                }
+            })
+        
+        })
+    })
+
+})
